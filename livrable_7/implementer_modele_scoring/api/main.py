443d3a1e2d66e25cfa256cfb258617a6cdc82724
maxime dupouy <maxime_dupouy@hotmail.fr>
2022-11-18 16:33:36 +0100

    api ok, streamlit ok

--- livrable_7/implementer_modele_scoring/api/main.py
+++ livrable_7/implementer_modele_scoring/api/main.py
@@ -1,6 +1,9 @@
 import pandas as pd
-from api.utils import load_model, load_data, get_client_data, client_id_in_data_test, get_sample_id, empty_data_response
-from config import MAX_INTEREST_COLS
+from api.loader import Loader
+from api.graphs import create_features_importance_model, create_box_plot, create_force_plot
+from api.utils import client_id_in_data_test, get_sample_id
+from config import EMPTY_RESPONSE, COLUMNS_ORDER
+from api.feature_imp import FeaturesImportance
 
 def get_features_importance(model,
                             features:list) -> pd.core.frame.DataFrame:
@@ -40,41 +43,69 @@ def predict_solvability(row_data:pd.core.frame.DataFrame,
                         model)->str:
     '''predict client solvability'''
     res, proba = model.predict(row_data), model.predict_proba(row_data)
-    return str(res[0]), str(proba[0])
+    return str(res[0]), str(max(proba[0]))
 
 
 def get_data_(params:str)->str:
-    client_id = int(params) # ['client_id']
-    data = load_data()
-    response_data = empty_data_response()
+    client_id = int(params)
+    data = Loader.load_data()
+    response_data = EMPTY_RESPONSE
 
     # essaye de retrouver la donnée pour le client_id donné
     index_not_found = client_id_in_data_test(data, int(client_id))
 
     if index_not_found:
-        # TODO index ) false dans response
         response_data['error']['status'] = index_not_found
         response_data['error']['client_id_sample'] = str(get_sample_id(data))
         return response_data
 
     else :
-        # TODO regarder importance de client_data
-        client_data = get_client_data(data, client_id)
-        data = data.drop(columns=['is_test'])
-        model = load_model()
-        features_imp = get_features_importance(model, client_data.columns)
+        data = data.drop(columns=['TARGET'])
+        model = Loader.load_model()
+
+        # calculte client importance fearure with shap
+        f_i = FeaturesImportance(model, data.columns)
+        shap_values = f_i.get_shap_values(data[data.index==client_id])
+        features_imp = f_i.get_feature_importance(shap_values)
         data_to_build = get_limit_values(features_imp, data, client_id)
-        # top_features = features_imp.iloc[:MAX_INTEREST_COLS, :]
+
         response_data['error']['status'] = index_not_found
         response_data['data'] = data_to_build.to_json()
         return response_data
 
 def make_prediction(client_data:dict):
-    model = load_model()
-    print('_________________')
-    print(client_data)
-    client_data = pd.DataFrame.from_dict(client_data, orient='index').transpose()
-    print(client_data.head())
+    model = Loader.load_model()
+    client_data = pd.DataFrame.from_dict(
+        client_data, orient='index').transpose()[COLUMNS_ORDER]
     prediction, probability = predict_solvability(client_data, model)
+
     return {"prediction" : prediction,
             "probabilies" : probability,}
+
+def make_butifuls_graphs(graph_params):
+
+    data = Loader.load_data()
+    model = Loader.load_model()
+
+    if graph_params['which_graph'] == 'features_importance_model':
+        return create_features_importance_model(model,
+                                                data.columns.drop('TARGET')
+                                                )
+
+    if graph_params['which_graph'] == 'client_distribution':
+        feature_client_value = float(
+            graph_params['data_client'][graph_params['feature']]
+            )
+        return create_box_plot(data,
+                               graph_params['feature'],
+                               feature_client_value)
+
+    if graph_params['which_graph'] == 'force_plot':
+        client_data = pd.DataFrame.from_dict(
+            graph_params['data_client'], orient='index'
+            ).transpose()[COLUMNS_ORDER]
+        return create_force_plot(client_data,
+                                 model,
+                                 data.columns.drop('TARGET'),
+                                 int(graph_params['prediction'])
+                                )
--- livrable_7/implementer_modele_scoring/frontend/main.py
+++ livrable_7/implementer_modele_scoring/frontend/main.py
@@ -1,75 +1,175 @@
+import io
 import json
+
 import pandas as pd
 import requests
 import streamlit as st
+
 from config import HOST, PORT
+from frontend.utils import create_gauge_plot
 
-payload = {
-    "client_id": None
-}
 
-## TITLE
-st.title('Hello World')
+## Load desc
+f = open('./data/columns_descriptions.json')
+columns_descriptions = json.load(f)
+f = open('./data/multiples_descriptions.json')
+multiple_desc = json.load(f)
 
-## SIDE BAR INPUT CLIENT ID
+
+st.set_page_config(layout="wide")
+
+
+########################################### TITLE ###########################################
+title = '<b><p style="font-family:sans-serif; font-size: 45px;text-align: center;">🔮 Prêt-à-dépenser 🔮</b></p>'
+st.markdown(title, unsafe_allow_html=True)
+st.markdown(4 * "<br />", unsafe_allow_html=True)
+
+########################################### SIDEBAR INPUTS ###########################################
+
 client_id = st.sidebar.number_input('Insert client id',
-                                            value=0)
-payload["client_id"] = str(client_id)
+                                    value=0)
+payload = {
+    "client_id": str(client_id)
+    }
 
 ## GET DATA
 response = requests.post(''.join([HOST, PORT,'/get_data/']),
                          json=payload
                          ).json()
 
-## CHECK STATUS
-error = response["error"]["status"]
+## SITE DE BASE
+if response["error"]["status"] and str(client_id) == "0":
 
-## IF INPUT ID ISNT KNOWN
-if error :
-    st.write("Cet identifiant ne correspond à aucun identifiant de client dans la base de donnée 😱")
+    st.write(f'{multiple_desc["site_desc"]}')
+    st.write(" ")
+    st.write(multiple_desc["data_desc"])
+
+## ID NOT FOUND
+elif response["error"]["status"] and str(client_id) != "0":
+    st.write('<p style="font-family:sans-serif; font-size: 25px;text-align: center;">⚠️ ERREUR ⚠️</p>', unsafe_allow_html=True)
+    st.write(" ")
+    st.write(" ")
+    st.write('Cet identifiant ne correspond à __aucun identifiant de client__ dans la base de donnée 😱😱😱')
     st.write("Exemples d'idenfiants connus :")
     st.write(', '.join([str(i) for i in eval(response["error"]["client_id_sample"])]))
 
-## ELSE : LET THE MAGIE BEGGINS
-else :
-    # GET DF DATA FROM RESPONSE
-    data = pd.DataFrame(eval(response["data"]))
 
-    #GET CLIENT DATA
+## LET THE MAGIE BEGGINS
+else :
+
+    # DATA_df FROM RESPONSE
+    data = pd.DataFrame(eval(response["data"]))
+    # CLIENT DATA
     client_value = data.loc[:, 'client_value']
 
-    # INPUT HOWMANY FEATURE TO ANALYSED (N)
-    number_features_to_eval = st.sidebar.slider(
-        'Insert range of features you want to analyse',
-        min_value=0,
-        max_value=5, # crash si trop de colonnes
-        value=0)
+    # INPUT HOWMANY FEATURE TO ANALYSED
+    st.sidebar.write('______________________________')
+    st.sidebar.write(' ')
 
-    ## ITERE SUR LES N FEATURES AYANT LE + D'IOMPACT
+    number_features_to_eval = int(st.sidebar.selectbox(
+        'Features range you want to analyse',
+        ["2", "3", "4"]))
+
+    st.sidebar.write('______________________________')
+    st.sidebar.write(' ')
+
+    # SLIDER FOR MOST IMPACTFUL FEATURES
+    st.sidebar.write(f'*Most {str(number_features_to_eval)} impactful features for selected client :*')
+
+    # iterate over n MOST IMPACTFUL FEATURES
     for feature_name, data_feature in data.iloc[:number_features_to_eval,
                                                 :].iterrows():
-        # SI ON VEUT LE DESCRIPTIF DE LA FEATURE
-        if st.sidebar.button(f'voir description de la colonne {feature_name}'):
-            st.sidebar.write('cest la description')
+        # possibility to click for feature description
+        if st.sidebar.button(f'Show {feature_name} description'):
+            st.sidebar.write(columns_descriptions[feature_name])
 
-        # CREER UN SIDEBAR POUR CHANGER INPUT DE LA FEATURE
+        # instanciate sidebar to input new feature value
         values = st.sidebar.slider(
-        f'Move value for {feature_name}',
+        f'Select value for {feature_name}',
         min_value=float(data.loc[feature_name, 'min_value']),
         max_value=float(data.loc[feature_name, 'max_value']),
         value=float(data.loc[feature_name, 'client_value'])
         )
+        # replace client_data value by feature input
         data.loc[feature_name, 'client_value'] = values
-        #st.write('Values:', values)
 
+
+    ########################################### PREDICTION ###########################################
     prediction = requests.post(''.join([HOST, PORT,'/predict/']),
-                               json=client_value.to_dict())
-    st.write('prediction :', prediction.json())
+                               json=client_value.to_dict()).json()
+    prediction, probalitie = prediction["prediction"], float(prediction['probabilies'])
 
-    with st.expander("See explanation"):
-        st.write("""
-            The chart above shows some numbers I picked for you.
-            I rolled actual dice for these, so they're *guaranteed* to
-            be random.
-        """)
-        st.write("Ici")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        to_display = multiple_desc[prediction]
+        title = f'<p style="font-family:sans-serif; font-size: 28px;text-align: center;">{to_display}</p>'
+        st.markdown(title, unsafe_allow_html=True)
+    with col2:
+        fig = create_gauge_plot(probalitie)
+        st.plotly_chart(fig)
+
+
+    ########################################### GRAPHS ###########################################
+    graph_params = {
+        "feature": None,
+        "data_client": client_value.to_dict(),
+        "client_id":client_id,
+        "which_graph": None,
+        "prediction": prediction if prediction else None
+    }
+
+    ######## FORCE PLOT
+    with st.expander("See features empact on prediction"):
+        st.write(f"Explanation for the {'credit acceptance' if prediction=='0' else 'refusal of credit'}.")
+        st.write(multiple_desc["force_plot_desc"])
+        graph_params['which_graph']='force_plot'
+        force_plot_graph = requests.get(''.join([HOST, PORT, '/']),
+                                                 json=graph_params)
+        with io.BytesIO(force_plot_graph.content) as f:
+            st.image(f, width=1000)
+
+    ######## FEATURE IMPORTANCE GRAPH
+    with st.expander("See models explanation"):
+        st.write(multiple_desc['feature_desc'])
+        # récupere le graph
+        graph_params['which_graph']='features_importance_model'
+        features_importance_model = requests.get(''.join([HOST, PORT, '/']),
+                                                 json=graph_params)
+        with io.BytesIO(features_importance_model.content) as f:
+            st.image(f, width=850)
+
+
+    ######## BOXPLOTS
+    with st.expander("See features distribution"):
+        st.write(multiple_desc["box_plot_desc"])
+
+        graph_params['which_graph']='client_distribution'
+
+        feature_to_boxplot = client_value.index[:number_features_to_eval]
+        graph_to_display = []
+        for ind, feature in enumerate(feature_to_boxplot):
+            graph_params["feature"] = feature
+            test = requests.get(''.join([HOST, PORT, '/']),
+                                            json=graph_params)
+            graph_to_display.append(test.content)
+
+        # UGLY PART TO DISPLAY BOXPLOTS IN COLUMNS
+        col1, col2 = st.columns(2)
+        with col1:
+            with io.BytesIO(graph_to_display[0]) as f:
+                st.image(f, width=500)
+            if len(graph_to_display) > 2:
+                with io.BytesIO(graph_to_display[2]) as f:
+                    st.image(f, width=500)
+        with col2:
+            with io.BytesIO(graph_to_display[1]) as f:
+                st.image(f, width=500)
+            if len(graph_to_display) == 4 :
+                with io.BytesIO(graph_to_display[3]) as f:
+                    st.image(f, width=500)
+
+
+
+st.markdown(10 * "<br />", unsafe_allow_html=True)
+st.write(multiple_desc["contact"])
