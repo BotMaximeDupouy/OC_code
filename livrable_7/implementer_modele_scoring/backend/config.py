564dd7e30c4f37486f4b849f698041f958f819d1
maxime dupouy <maxime_dupouy@hotmail.fr>
2022-12-09 18:05:17 +0100

    heroku little adjustement

--- livrable_7/implementer_modele_scoring/backend/config.py
+++ livrable_7/implementer_modele_scoring/backend/config.py
@@ -3,7 +3,7 @@ MODEL_TO_USE = "random_forest_final.pkl"
 #MODEL_TO_USE = "random_orest_no_finetuning.pkl"
 DATA_PATH = './data/df_complete.csv.gz'
 HOST = 'http://127.0.0.1:8000'
-HEROKU_HOST = 'scoring-credit-openclassroom-48975'
+HEROKU_HOST = 'https://scoring-credit-oc-48975.herokuapp.com'
 EMPTY_RESPONSE = {
     'error': {'status' : None,
               'client_id_sample' : None,
